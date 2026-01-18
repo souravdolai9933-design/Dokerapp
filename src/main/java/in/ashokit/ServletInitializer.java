0c21a9abac3d30e664089d33690bfd562abbfcb3
Sourav <souravdolai9933@gmail.com>
2026-01-18 19:22:36 +0530

    bug fixed

--- src/main/java/in/ashokit/ServletInitializer.java
+++ src/main/java/in/ashokit/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+                 int j =20;
 		return application.sources(SbRestAppApplication.class);
 	}
 
